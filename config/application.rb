0c7a475ee0d079ed1251cf27e5a01a56f4fcb6fd
Victor Motogna <vmotogna@gmail.com>
2019-12-26 18:53:13 +0200

    Re-edit application.rb config

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,22 @@
 require_relative 'boot'
-require 'rails/all'
 
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module GithubActionsDemo
   class Application < Rails::Application
     config.load_defaults 6.0
     config.api_only = true
-    config.autoload_paths += %W[#{config.root}/doc #{config.root}/lib]
   end
 end
